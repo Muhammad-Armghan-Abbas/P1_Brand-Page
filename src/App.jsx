3a37c654417eb3dd3b4cc7355d73d7141cbaa89a
Armaghan Abbas <armaghanabbas5@gmail.com>
2024-05-28 06:39:22 +0500

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import HeroSection from './componenets/hero'
+import Navigation from './componenets/navigation'
+function App() {
+  return (
+      <div>
+<Navigation/>
+<HeroSection/>
+      </div>
+  )
+}
+export default App
---
+++ src/componenets/navigation.jsx
@@ -0,0 +1,15 @@
+function Navigation() {
+  return (
+        <nav className="container">
+        <img src="/images/brand_logo.png" alt="logo" />
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contacts</li>
+        </ul>
+        <button>Login</button>
+        </nav>
+  )
+}
+export default Navigation
